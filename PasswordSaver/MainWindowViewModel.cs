1bad6d23c40d97eb44e96511286e3994229a1e7b
AnMal <junkie_@msn.com>
2023-11-01 20:59:41 +0100

    Add project files.

---
+++ PasswordSaver/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.IO;
+
+namespace PasswordSaver;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    private readonly string _filePath = "password.txt";
+    private readonly PasswordHasher _hasher = new();
+
+    [ObservableProperty] private string? _message;
+
+    [ObservableProperty, 
+        NotifyCanExecuteChangedFor(nameof(SavePasswordCommand)), 
+        NotifyCanExecuteChangedFor(nameof(VerifyPasswordCommand))]
+    private string? _password;
+
+    [RelayCommand(CanExecute = nameof(CanSavePassword))]
+    private void SavePassword()
+    {
+        var hashedPass = _hasher.HashPassword(Password!);
+
+        File.WriteAllText(_filePath, hashedPass);
+    }
+    private bool CanSavePassword()
+    {
+        return Password?.Length > 0;
+    }
+
+    [RelayCommand(CanExecute = nameof(CanVerifyPassword))]
+    private void VerifyPassword()
+    {
+        var hashedPass = File.ReadAllText(_filePath);
+        var isPassOk = _hasher.VerifyPassword(Password!, hashedPass);
+
+        if (isPassOk)
+        {
+            Message = "Lozinka je ispravna!";
+        }
+        else
+        {
+            Message = "Lozinka je neispravna!";
+        }
+    }
+    private bool CanVerifyPassword()
+    {
+        return Password?.Length > 0;
+    }
+}
---
+++ PasswordSaver/PasswordHasher.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+
+namespace PasswordSaver;
+
+public class PasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        var salt = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+
+        var hashedPass = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 32));
+
+        return $"{Convert.ToBase64String(salt)}.{hashedPass}";
+    }
+
+    public bool VerifyPassword(string password, string storedHash)
+    {
+        var passParts = storedHash.Split(".");
+
+        var hashedPass = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: Convert.FromBase64String(passParts[0]),
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 32));
+
+        return hashedPass == passParts[1];
+    }
+}
